41bfcf6abd18626cd4075a14d21ea785b70f3a32
informistica <mauro.spinarelli@gmail.com>
2020-05-14 19:02:39 +0200

    Setting per deploy

--- verifica/settings.py
+++ verifica/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+6c!**luf)wbhlzv+5iuc8m_s&s9arpz*ztxmauh+5=l^y%qdf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'verifica.urls'
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
