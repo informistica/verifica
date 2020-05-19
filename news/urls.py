692106f196864f0aff70abffb0f9f759fd1e181d
informistica <mauro.spinarelli@gmail.com>
2020-05-19 18:37:17 +0200

    Aggiunta lista articoli
    Solo di un giornalista

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import listaArticoliView,listaGiornalistiView
+from .views import listaArticoliView,listaGiornalistiView,listaArticoliGiornalistaView
 from . import views
 
 app_name = 'news'
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('lista-giornalisti',  listaGiornalistiView.as_view(), name='lista_giornalisti'),
     path('lista-articoli', listaArticoliView.as_view(), name='lista_articoli'),
+    path('lista-articoli-giornalista/<int:pk>', listaArticoliGiornalistaView, name='lista_articoli_giornalista'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Articolo,Giornalista
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def home(request):
@@ -15,6 +16,14 @@ class listaArticoliView(ListView):
         context["articoli"] = Articolo.objects.all()
         return context
 
+def listaArticoliGiornalistaView(request,pk):
+    giornalista=get_object_or_404(Giornalista, id=pk)
+    articoli=giornalista.articoli.all()
+    context = {'giornalista': giornalista,
+               'articoli': articoli,
+               }
+    return render(request, 'news/articoli.html', context)
+
 class listaGiornalistiView(ListView):
     model = Giornalista
     template_name = 'news/giornalisti.html'
@@ -22,3 +31,4 @@ class listaGiornalistiView(ListView):
         context = super().get_context_data(**kwargs)
         context["giornalisti"] = Giornalista.objects.all()
         return context
+
